304f197e608ce9b0e25f1e44916d3bc7e3021818
Tanvir Hossain <tanvir@disystem.net>
2024-11-09 15:35:47 +0600

    Slider add and css

--- src/components/pageComponent/Slider/Slider.jsx
+++ src/components/pageComponent/Slider/Slider.jsx
@@ -4,29 +4,37 @@ import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 
 
 const Slider = () => {
-    const [currentSlide,setcurrentSlide] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    const data = [
-        "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      ];
+  const data = [
+    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
 
-      const prevSlide = ()=>{
-        console.log("Hello")
-        setcurrentSlide(currentSlide === 0 ? 2 : (prev)=> prev - 1);
-      };
-      const nextSlide = ()=>{
-        setcurrentSlide(currentSlide === 2 ? 0 : (prev)=> prev + 1);
-      }
-    return (
-        <div className="slider">
-      <div className="container" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
-        <img src={data[0]} alt="" />
-        <img src={data[1]} alt="" />
-        <img src={data[2]} alt="" />
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? data.length - 1 : currentSlide - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === data.length - 1 ? 0 : currentSlide + 1);
+  };
+
+  return (
+    <div className="slider">
+      <div
+        className="slider-container"
+        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+      >
+        {data.map((img, index) => (
+          <div key={index} className="slide">
+            <img src={img} alt={`slide-${index}`} className="slider-image" />
+          </div>
+        ))}
       </div>
-      <div className="icons">
+      <div className="slider-icons">
         <div className="icon" onClick={prevSlide}>
           <WestOutlinedIcon />
         </div>
@@ -34,8 +42,17 @@ const Slider = () => {
           <EastOutlinedIcon />
         </div>
       </div>
+      <div className="slider-dots">
+        {data.map((_, index) => (
+          <div
+            key={index}
+            className={`dot ${index === currentSlide ? "active" : ""}`}
+            onClick={() => setCurrentSlide(index)}
+          />
+        ))}
+      </div>
     </div>
-    );
+  );
 };
 
 export default Slider;
